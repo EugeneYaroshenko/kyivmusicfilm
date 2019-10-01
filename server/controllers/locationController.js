d4a421427c70d8ff71c53e88214156dd5cd8ed83
eugene.yaroshenko <Eugene.Yaroshenko@securekey.com>
2019-10-01 19:19:16 +0300

    updated server

--- server/controllers/locationController.js
+++ server/controllers/locationController.js
@@ -2,10 +2,12 @@ const consola = require('consola')
 const axios = require('axios')
 
 exports.location = async function (req, res) {
+
   try {
     const geolocationAPI = 'https://geoip-db.com/json/'
 
     const result = await axios.get(geolocationAPI)
+    consola.info('here', result.data)
 
     res.status(200).send({ geoLocation: result.data })
   } catch (e) {
